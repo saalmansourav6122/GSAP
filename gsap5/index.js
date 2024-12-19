0aae078acc63967b7cc75c77167d70252eaadc78
saalmansourav6122 <saalmansourav111@gmail.com>
2024-12-19 17:07:56 +0600

    Its worked perfectly

--- gsap5/index.js
+++ gsap5/index.js
@@ -1,12 +1,20 @@
+let hamburger = document.querySelector(".nav i");
+let cross = document.querySelector("ul i");
 let tl = gsap.timeline();
+
 tl.to("ul", {
   right: 0,
-  duration: 1,
-  delay: 1,
 });
 tl.from("ul li", {
-  x: 150,
+  x: 100,
   duration: 0.3,
-  stagger: 0.8,
+  stagger: 0.2,
   opacity: 0,
+});
+tl.pause();
+hamburger.addEventListener("click", () => {
+  tl.play();
+});
+cross.addEventListener("click", () => {
+  tl.reverse();
 });
